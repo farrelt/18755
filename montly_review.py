22149e1df2b5033e4463f3554753516c8d1dbf23
Yi Fan <yif@weh5201-c5.andrew.cmu.edu>
2018-10-31 14:27:09 -0400

    Update montly_review.py

--- montly_review.py
+++ montly_review.py
@@ -2,7 +2,7 @@ import json
 import os
 from dateutil.parser import parse
 
-HOMEDIR = "C:/Users/Farreltin/Documents/fall2018/CourseData/"
+HOMEDIR = "/Users/yif/Documents/GitHub/18755/"
 
 folderPath1 = HOMEDIR + "result/" + '2016-01-01'
 folderPath2 = HOMEDIR + "result/" + '2016-07-01'
@@ -10,13 +10,16 @@ folderPath3 = HOMEDIR + "result/" + '2017-01-01'
 folderPath4 = HOMEDIR + "result/" + '2017-07-01'
 
 
-def getRev(date1, date2):
+def getRev():
 
     date1 = parse('2016/01/01')
     date2 = parse('2016/07/01')
     date3 = parse('2017/01/01')
     date4 = parse('2017/07/01')
     date5 = parse('2018/01/01')
+
+    print(folderPath1)
+    
 
     os.makedirs(folderPath1)
     os.makedirs(folderPath2)
@@ -26,7 +29,7 @@ def getRev(date1, date2):
 
     file = 'yelp_academic_dataset_review.json'
     print("loading all the review......")
-    for line in open(HOMEDIR + file, 'r', encoding="utf8"):
+    for line in open(HOMEDIR + file, 'r'):
         r = json.loads(line)
         date = parse(r["date"])
 
@@ -34,23 +37,28 @@ def getRev(date1, date2):
             if date < date2:
                 with open(folderPath1 + '/data.json', 'a') as fp:
                     json.dump(r, fp)
+                    fp.write('\n')
             elif date < date3:
                 with open(folderPath2 + '/data.json', 'a') as fp:
                     json.dump(r, fp)
+                    fp.write('\n')
             elif date < date4:
                 with open(folderPath3 + '/data.json', 'a') as fp:
                     json.dump(r, fp)
+                    fp.write('\n')
             elif date < date5:
                 with open(folderPath4 + '/data.json', 'a') as fp:
                     json.dump(r, fp)
+                    fp.write('\n')
+
+    print("finished!")
     return
 
 def reduce_review():
     print('start time window 1')
-    for line in open(folderPath1 + '/data.json', 'r', encoding="utf8"):
-        review_count = dict()
+    review_count = dict()
+    for line in open(folderPath1 + '/data.json', 'r'):
         r = json.loads(line)
-        #print(r)
         busid = r["business_id"]
         if busid in review_count:
             (info, count) = review_count[busid]
@@ -64,18 +72,24 @@ def reduce_review():
     final_review = dict()
     for res in review_count:
         (info, count) = review_count[res]
-        if count > 100:
+        print(count)
+        if count > 30:
             final_review[res] = info
     print("finished reducing")
 
     print("dumping in a json file....")
-    with open(folderPath1 + '/reduced_data.json', 'w') as fp:
-        json.dump(final_review, fp)
+    for e in final_review:
+        (info, count) = review_count[e]
+        for e in info:
+            with open(folderPath1 + '/reduced_data.json', 'a') as fp:
+                json.dump(e, fp)
+                fp.write('\n')
     print("finish dumping")
 
     print("start time window2 ...")
-    for line in open(folderPath2 + '/data.json', 'r', encoding="utf8"):
-        review_count = dict()
+    review_count = dict()
+    for line in open(folderPath2 + '/data.json', 'r'):
+        
         r = json.loads(line)
         #print(r)
         busid = r["business_id"]
@@ -91,18 +105,23 @@ def reduce_review():
     final_review = dict()
     for res in review_count:
         (info, count) = review_count[res]
-        if count > 100:
+        if count > 30:
             final_review[res] = info
     print("finished reducing")
 
     print("dumping in a json file....")
-    with open(folderPath2 + '/reduced_data.json', 'w') as fp:
-        json.dump(final_review, fp)
+    for e in final_review:
+        (info, count) = review_count[e]
+        for e in info:
+            with open(folderPath2 + '/reduced_data.json', 'a') as fp:
+                json.dump(e, fp)
+                fp.write('\n')
     print("finish dumping")
 
     print('start time window3')
-    for line in open(folderPath3 + '/data.json', 'r', encoding="utf8"):
-        review_count = dict()
+    review_count = dict()
+    for line in open(folderPath3 + '/data.json', 'r'):
+        
         r = json.loads(line)
         #print(r)
         busid = r["business_id"]
@@ -115,22 +134,26 @@ def reduce_review():
             review_count[busid] = ([r], 1)
 
     print("reducing the number of comments...")
-    final_review = dict()
+    final_review = []
     for res in review_count:
         (info, count) = review_count[res]
         if count > 100:
-            final_review[res] = info
+            final_review.extend(info)
     print("finished reducing")
 
+    print(final_review)
     print("dumping in a json file....")
-    with open(folderPath3 + '/reduced_data.json', 'w') as fp:
-        json.dump(final_review, fp)
+    for e in final_review:
+        print(e)
+        with open(folderPath3 + '/reduced_data.json', 'a') as fp:
+            json.dump(e, fp)
+            fp.write('\n')
     print("finish dumping")
 
     print('start time window4')
-
-    for line in open(folderPath4 + '/data.json', 'r', encoding="utf8"):
-        review_count = dict()
+    review_count = dict()
+    for line in open(folderPath4 + '/data.json', 'r'):
+        
         r = json.loads(line)
         #print(r)
         busid = r["business_id"]
@@ -146,16 +169,46 @@ def reduce_review():
     final_review = dict()
     for res in review_count:
         (info, count) = review_count[res]
-        if count > 100:
+        if count > 30:
             final_review[res] = info
     print("finished reducing")
 
     print("dumping in a json file....")
-    with open(folderPath4 + '/reduced_data.json', 'w') as fp:
-        json.dump(final_review, fp)
+    for e in final_review:
+        (info, count) = review_count[e]
+        for e in info:
+            with open(folderPath4 + '/reduced_data.json', 'a') as fp:
+                json.dump(e, fp)
+                fp.write('\n')
     print("finish dumping")
 
     return
+
+def com2txt():
+    business_c = []
+    print("writing comments to txt ...")
+
+    
+    for line in open(folderPath3 + '/reduced_data.json', 'r'):
+        
+        r = json.loads(line)
+        for e in r:
+            print(e)
+        #busid = r["business_id"]
+        #rid = r["review_id"]
+        #text = r["text"]
+        #if not os.path.exists(folderPath1 + '/' + busid):
+         #   os.makedirs(busid)
+        #osPath = folderPath1 + '/' + busid + '/comment[' + rid + '].txt'
+        #with open(osPath, 'a') as file:
+        #    file.write('%s\n' % text.encode('utf-8'))
+
+          
+          
+            
+
+    return
+
 '''
         if (parse(r["date"]) > parse(date1)) and (parse(r["date"]) < parse(date2)):
             busid = r["business_id"]
@@ -196,10 +249,8 @@ def reduce_review():
                 file.write("%s" % text.encode('utf-8'))
 
     print("finished!")'''
-
-
-getRev('2017/01/01', '2017/07/01')
-
+reduce_review()
+#com2txt()
  #folderPath = "C:/Users/Farreltin/Documents/fall2018/CourseData/result/" + busid
             #if not os.path.exists(folderPath):
             #    os.makedirs(folderPath)
